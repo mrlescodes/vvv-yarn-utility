da622e30501768aae6a5b2427c9a4e36a963804b
Les Aubrey <hello@mrles.co.uk>
2020-12-14 10:50:27 +0700

    Replace apt with apt-get in provision script

--- yarn/provision.sh
+++ yarn/provision.sh
@@ -7,4 +7,5 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Update packages and install yarn
-sudo apt update && sudo apt install yarn
+sudo apt-get update &>/dev/null
+sudo apt-get install yarn &>/dev/null
