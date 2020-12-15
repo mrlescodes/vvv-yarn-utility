8b1e4bdaa65602b1897b696547a4058c77418892
Les Aubrey <hello@mrles.co.uk>
2020-12-15 14:48:53 +0700

    Fix: apt-key output should not be parsed (stdout is not a terminal) warning

--- yarn/provision.sh
+++ yarn/provision.sh
@@ -3,7 +3,7 @@
 echo "Installing Yarn"
 
 # Configure the repository
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo apt-key adv --fetch-keys https://dl.yarnpkg.com/debian/pubkey.gpg
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Update packages and install yarn
